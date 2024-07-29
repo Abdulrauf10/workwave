4821e9b53158f112651b5ba6b8330a2d4828b54f
Abdulrauf10 <abd.rauf.lamada@gmail.com>
2024-07-29 10:51:21 +0700

    change ui

--- src/features/JobList.tsx
+++ src/features/JobList.tsx
@@ -94,7 +94,7 @@ export default function JobList() {
           </InputGroup>
 
           <Image
-            src="/workwave.png"
+            src="/vercel.svg"
             alt="workwave"
             width={"50px"}
             height={"50px"}
@@ -102,125 +102,126 @@ export default function JobList() {
           />
         </Box>
 
-        {dataJobs ? (
-          <>
-            <Box
-              width={"49%"}
-              bg={"#0C66C2"}
-              display={"flex"}
-              alignItems={"flex-start"}
-              flexDir={"column"}
-              justifyContent={"center"}
-              padding={"5px"}
-              mt={"10px"}
-              mb={"10px"}
+        <>
+          <Box
+            width={"49%"}
+            bg={"#0C66C2"}
+            display={"flex"}
+            alignItems={"flex-start"}
+            flexDir={"column"}
+            justifyContent={"center"}
+            padding={"5px"}
+            mt={"10px"}
+            mb={"10px"}
+          >
+            <Text
+              ml={"10px"}
+              fontSize={"18px"}
+              color={"white"}
+              fontWeight={500}
             >
-              <Text
-                ml={"10px"}
-                fontSize={"18px"}
-                color={"white"}
-                fontWeight={500}
-              >
-                Top job Picks for you
-              </Text>
-              <Text color={"white"} ml={"10px"}>
-                {dataJobs?.pages[0]?.data?.length
-                  ? `${dataJobs?.pages[0]?.data?.length} result`
-                  : "0 result"}
-              </Text>
-            </Box>
-            <Grid
-              templateColumns={{ base: "1fr", md: "1fr 1fr" }}
-              gap={6}
-              boxShadow={"md"}
-              pb={"7px"}
-            >
-              <GridItem maxH={"60vh"} overflow={"auto"}>
-                {dataJobs?.pages?.map((comp: any) =>
-                  comp?.data?.map((data: any, key: any) => (
-                    <Box
-                      key={key}
-                      p={"10px"}
-                      border={"1px solid #ccc"}
-                      mb={"10px"}
-                      cursor={"pointer"}
-                      onClick={() => setSelectedJob(data)}
-                      display={"flex"}
-                      alignItems={"center"}
-                    >
-                      <Skeleton isLoaded={!loading}>
-                        <Image
-                          src={data?.employer_logo || "/workwave.png"}
-                          alt="employer"
-                          height={35}
-                          width={35}
-                        />
-                      </Skeleton>
-                      <Skeleton isLoaded={!loading} ml={"12px"}>
-                        <Text
-                          color={"#0C66C2"}
-                          fontSize={"15px"}
-                          fontWeight={700}
-                        >
-                          {data?.job_title}
-                        </Text>
-                        <Text fontSize={"12px"} fontWeight={400}>
-                          {data?.employer_name}
-                        </Text>
-                        <Text
-                          color={"#666666"}
-                          fontSize={"12px"}
-                          fontWeight={400}
-                        >
-                          {data?.job_city || "(Remote)"},{" "}
-                          {data?.job_country || ""}
-                        </Text>
-                      </Skeleton>
-                    </Box>
-                  ))
-                )}
-              </GridItem>
-
-              <GridItem
-                overflow={"auto"}
-                maxH={"60vh"}
-                mt={{ base: "10px", md: "-40px" }}
-              >
-                {selectedJob ? (
-                  <Box p={"20px"} border={"1px solid #ccc"}>
-                    <Image
-                      src={selectedJob?.employer_logo || "/workwave.png"}
-                      alt="employer"
-                      height={25}
-                      width={25}
-                    />
-                    <Text fontSize={"2xl"}>{selectedJob?.job_title}</Text>
-                    <Text fontSize={"12px"} fontWeight={400}>
-                      {selectedJob?.job_country} . posted:{" "}
-                      {formatDate(selectedJob?.job_posted_at_datetime_utc) ||
-                        "2 weeks ago"}
-                    </Text>
-                    <Link href={selectedJob.job_apply_link} target="_blank">
-                      <Button
-                        color={"white"}
-                        bg={"#0C66C2"}
-                        width={"150px"}
-                        borderRadius={"20px"}
-                        mb={"30px"}
-                        mt={"15px"}
+              Top job Picks for you
+            </Text>
+            <Text color={"white"} ml={"10px"}>
+              {dataJobs?.pages[0]?.data?.length
+                ? `${dataJobs?.pages[0]?.data?.length} result`
+                : "0 result"}
+            </Text>
+          </Box>
+          <Grid
+            templateColumns={{ base: "1fr", md: "1fr 1fr" }}
+            gap={6}
+            boxShadow={"md"}
+            pb={"7px"}
+          >
+            <GridItem maxH={"60vh"} overflow={"auto"}>
+              {dataJobs?.pages?.map((comp: any) =>
+                comp?.data?.map((data: any, key: any) => (
+                  <Box
+                    key={key}
+                    p={"10px"}
+                    border={"1px solid #ccc"}
+                    mb={"10px"}
+                    cursor={"pointer"}
+                    onClick={() => setSelectedJob(data)}
+                    display={"flex"}
+                    alignItems={"center"}
+                  >
+                    <Skeleton isLoaded={!loading}>
+                      <Image
+                        src={data?.employer_logo || "/vercel.svg"}
+                        alt="employer"
+                        height={35}
+                        width={35}
+                      />
+                    </Skeleton>
+                    <Skeleton isLoaded={!loading} ml={"12px"}>
+                      <Text
+                        color={"#0C66C2"}
+                        fontSize={"15px"}
+                        fontWeight={700}
                       >
-                        Apply
-                      </Button>
-                    </Link>
-                    <Text mb={"10px"}>{selectedJob.job_description}</Text>
+                        {data?.job_title}
+                      </Text>
+                      <Text fontSize={"12px"} fontWeight={400}>
+                        {data?.employer_name}
+                      </Text>
+                      <Text
+                        color={"#666666"}
+                        fontSize={"12px"}
+                        fontWeight={400}
+                      >
+                        {data?.job_city || "(Remote)"},{" "}
+                        {data?.job_country || ""}
+                      </Text>
+                    </Skeleton>
                   </Box>
-                ) : (
-                  <Box p={"20px"} border={"1px solid #ccc"}>
-                    <Text>Please select a job to see the details</Text>
-                  </Box>
-                )}
-              </GridItem>
-            </Grid>
+                ))
+              )}
+            </GridItem>
+
+            <GridItem
+              overflow={"auto"}
+              maxH={"70vh"}
+              mt={{ base: "10px", md: "-70px" }}
+            >
+              {selectedJob ? (
+                <Box p={"20px"} border={"1px solid #ccc"}>
+                  <Image
+                    src={selectedJob?.employer_logo || "/vercel.svg"}
+                    alt="employer"
+                    height={25}
+                    width={25}
+                  />
+                  <Text fontSize={"2xl"}>{selectedJob?.job_title}</Text>
+                  <Text fontSize={"12px"} fontWeight={400}>
+                    {selectedJob?.job_country} . posted:{" "}
+                    {formatDate(selectedJob?.job_posted_at_datetime_utc) ||
+                      "2 weeks ago"}
+                  </Text>
+                  <Link href={selectedJob.job_apply_link} target="_blank">
+                    <Button
+                      color={"white"}
+                      bg={"#0C66C2"}
+                      width={"150px"}
+                      borderRadius={"20px"}
+                      mb={"30px"}
+                      mt={"15px"}
+                    >
+                      Apply
+                    </Button>
+                  </Link>
+                  <Text mb={"10px"}>{selectedJob.job_description}</Text>
+                </Box>
+              ) : (
+                <Box p={"20px"} border={"1px solid #ccc"}>
+                  <Text>Please select a job to see the details</Text>
+                </Box>
+              )}
+            </GridItem>
+          </Grid>
+
+          {dataJobs ? (
             <Box
               width={"100%"}
               display={"flex"}
@@ -245,17 +246,10 @@ export default function JobList() {
                 </TabList>
               </Tabs>
             </Box>
-          </>
-        ) : (
-          <Box
-            width={"100%"}
-            display={"flex"}
-            justifyContent={"center"}
-            marginTop={"200px"}
-          >
-            <Text>No job to display</Text>
-          </Box>
-        )}
+          ) : (
+            <></>
+          )}
+        </>
       </Box>
     </>
   )
